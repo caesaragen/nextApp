fd92d8f83a07d7385988e46d349fbcaea898eae5
Joel Aduma <caesaragen@gmail.com>
2022-04-26 15:35:00 +0300

    two steps forward, three backwards

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { About, Navbar } from '../components'
+
 
 
 export default function Home() {
   return (
-    <div>
-      Home
-</div>
+    <div className="md:container md:mx-auto flex">
+      <About/>
+    </div>
   )
 }
