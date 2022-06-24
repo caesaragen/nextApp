1eb4dadb582675eecfc13ef158cabf94ab738a48
Joel Aduma <caesaragen@gmail.com>
2022-06-24 12:28:42 +0300

    add background animation

--- components/About.jsx
+++ components/About.jsx
@@ -5,55 +5,78 @@ import { FaLinkedin } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import { FaFacebook } from 'react-icons/fa';
 import { FaWhatsapp } from 'react-icons/fa';
+import Image from "next/image";
+import profilePic from '../public/profilePic.jpg';
+import { FaReact } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { VscChecklist } from "react-icons/vsc";
+import { FaPlay } from "react-icons/fa";
+import { MoonIcon } from "@heroicons/react/solid";
+import { SunIcon } from "@heroicons/react/solid";
+import { FaLaravel } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3 } from "react-icons/fa";
+import { FaJs } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { FaPhp } from "react-icons/fa";
 
 
 
-const About = () => {
+
+const About = ({ toggle, setToggle }) => {
     return (
-        <div className='flex flex-col justify-center items-center p-4 border-2 border-gray-700 shadow-sm'>
-            <div className='flex flex-col justify-center items-center'>
-                <p className='text-xl text-white text-white text-center'>Hello, I am a frontend developer with a keen eye for building custom components to match designs and client expectations.I am experienced in HTML5/CSS3, Bootstrap, Javascript, React, React Native and JQuery.
-                    I am also proficient in consuming REST APIs as well as some background in backend development using Node (MERN) and PHP/Laravel (LAMP) </p>
+        <div className="md:w-[100rem] md:h-[40rem] w-[20.5rem] h-[40rem] p-4 rounded-2xl bg-white dark:bg-slate-800 shadow-lg dark:shadow-slate-700 flex flex-col ease-linear duration-300 md:flex-row-reverse">
+            <div className=" h-full w-full  shadow-md rounded-2xl basis-2/3 relative">
+                <div className=" text-white z-10 bg-[#5865F2] absolute pl-8 pr-8 pb-2 pt-2  rounded-tl-2xl rounded-br-2xl font-semibold">
+                    <h1>Joel</h1>
+                </div>
+                <div className="h-full w-full relative border-2 border-white rounded-2xl">
+                    <Image
+                        src={profilePic}
+                        alt="thumbnail"
+                        layout="fill"
+                        objectFit="cover"
+                        className=" rounded-2xl"
+                    />
+                </div>
             </div>
-         
-            <div className='flex flex-col justify-center items-center'>
-                <h1 className='text-2xl font-bold text-white'>Social Media</h1>
-                <div className='flex flex-row justify-center items-center gap-px'>
-                    <p className='text-xl text-white'><a href="
 
-        " className='text-blue-500 hover:text-blue-700'>
-                        <FaFacebook className='text-xl text-white' />
-                        <span className='ml-2'>
-                            <span className='font-bold'>    </span>
-                        </span>     </a></p>
-                    <p className='text-xl text-white'> <a href="
+            <div className=" h-full w-full mr-2 rounded-2xl ">
+                <p className="m-2 font-bold pl-1 text-lg text-[#5865F2]">About</p>
+                <h1 className="m-2 text-4xl font-bold dark:text-white">
+                    Software Developer
+                </h1>
 
-        " className='text-blue-500 hover:text-blue-700'>
-                        <FaTwitter className='text-xl text-white' />
-                        <span className='ml-2'>
-                            <span className='font-bold'>    </span>
-                        </span>     </a></p>
-                    <p className='text-xl text-white'> <a href="
-
-        " className='text-blue-500 hover:text-blue-700'>
-                        <FaLinkedin className='text-xl text-white' />
-                        <span className='ml-2'>
-                            <span className='font-bold'>    </span>
-                        </span>     </a></p>
-                    <p className='text-xl text-white'> <a href="
-
-        " className='text-blue-500 hover:text-blue-700'>
-                        <FaGithub className='text-xl text-white' />
-                        <span className='ml-2'>
-                            <span className='font-bold'>    </span>
-                        </span>     </a></p>
-                    <p className='text-xl text-white'> <a href="
-
-        " className='text-blue-500 hover:text-blue-700'>
-                        <FaWhatsapp className='text-xl text-white' />
-                        <span className='ml-2'>
-                            <span className='font-bold'>    </span>
-                        </span>     </a></p>
+                <div className=" pt-16 pr-2 pl-2 flex flex-row justify-around flex-wrap">
+                    <div className="flex flex-row items-center m-2">
+                        <FaReact size={20} color="#61DBFB" />
+                        <h1 className="pl-1 dark:text-white">ReactJs</h1>
+                    </div>
+                    <div className="flex flex-row items-center m-2">
+                        <FaLaravel size={20} color="#61DBFB" />
+                        <h1 className="pl-1 dark:text-white">Laravel</h1>
+                    </div>
+                    <div className="flex flex-row items-center m-2">
+                        <FaNodeJs size={20} color="#61DBFB" />
+                        <h1 className="pl-1 dark:text-white">NodeJs</h1>
+                    </div>
+                    <div className="flex flex-row items-center m-2">
+                        <FaJs size={20} color="#007acc" />
+                        <h1 className="pl-1 dark:text-white">JavaScript</h1>
+                    </div>
+                    <div className="flex flex-row items-center m-2">
+                        <FaHtml5 size={20} color="#007acc" />
+                        <h1 className="pl-1 dark:text-white">HTML</h1>
+                    </div>
+                    <div className="flex flex-row items-center m-2">
+                        <FaCss3 size={20} color="#007acc" />
+                        <h1 className="pl-1 dark:text-white">CSS</h1>
+                    </div>
+                    <div className="flex flex-row items-center m-2">
+                        <FaPhp size={20} color="#007acc" />
+                        <h1 className="pl-1 dark:text-white">PHP</h1>
+                    </div>
                 </div>
             </div>
         </div>
--- components/Layout.js
+++ components/Layout.js
@@ -4,8 +4,47 @@ import Image from 'next/image'
 import styles from '../styles/Layout.module.css'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useState } from 'react'
+import dynamic from 'next/dynamic';
 
 const Layout = ({ children }) => {
+    const ParticlesBg = dynamic(() => import('particles-bg'), {
+        ssr: false,
+    });
+
+    const [toggle, setToggle] = useState(false);
+    let config = {
+        num: [24, 27],
+        rps: 0.1,
+        radius: [5, 40],
+        life: [1.5, 3],
+        v: [2, 3],
+        tha: [-40, 40],
+        alpha: [0.6, 0],
+        scale: [.1, 0.4],
+        position: "all",
+        color: ["random", "#ff0000"],
+        cross: "dead",
+        // emitter: "follow",
+        random: 15
+    };
+
+    if (Math.random() > 0.85) {
+        config = Object.assign(config, {
+            onParticleUpdate: (ctx, particle) => {
+                ctx.beginPath();
+                ctx.rect(
+                    particle.p.x,
+                    particle.p.y,
+                    particle.radius * 2,
+                    particle.radius * 2
+                );
+                ctx.fillStyle = particle.color;
+                ctx.fill();
+                ctx.closePath();
+            }
+        });
+    }
     return (
         <div className={styles.container}>
             <Head>
@@ -15,8 +54,14 @@ const Layout = ({ children }) => {
             </Head>
             <Navbar />
             <main className={styles.main}>
-              
-                {children}
+                <ParticlesBg type="custom" config={config} bg={{
+                    position: "absolute",
+                    zIndex: 999,
+                }} />
+                <body style={{ zIndex:1000 }}>
+                    {children} 
+              </body>
+                
             </main>
 
 <Footer/>
